2654b5effe18c4841235ab066d262f9430ab6d61
kelvinchoi <kelvinchoi@naver.com>
2023-03-08 00:59:59 +0900

    first commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# SocialMediaFeed_Project
---
+++ SocialMediaFeed_Project/SocialMediaFeed_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialMediaFeed_Project
+//
+//  Created by 최형민 on 2023/03/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
