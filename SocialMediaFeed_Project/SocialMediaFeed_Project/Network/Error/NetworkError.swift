335490f2121facfbbdc69f932d0cf2dc86d6d210
kelvinchoi <kelvinchoi@naver.com>
2023-03-08 17:24:15 +0900

    feat: NetworkError Model 추가

---
+++ SocialMediaFeed_Project/SocialMediaFeed_Project/Network/Error/NetworkError.swift
@@ -0,0 +1,25 @@
+//
+//  NetworkError.swift
+//  SocialMediaFeed_Project
+//
+//  Created by 최형민 on 2023/03/08.
+//
+
+import Foundation
+
+enum NetworkError: LocalizedError {
+    case unknownError
+    case decodeError
+    case unstableNetworkError
+    
+    var errorDescription: String? {
+        switch self {
+        case .unknownError:
+            return "알 수 없는 에러 발생"
+        case .decodeError:
+            return "디코딩 에러 발생"
+        case .unstableNetworkError:
+            return "불안정한 네트워크 상태로 인한 에러 발생"
+        }
+    }
+}
